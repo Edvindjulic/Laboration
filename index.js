72cc68b0543e0e7db9c79bde10a55fdea8f59dee
edvindjulic <djulicedvin@gmail.com>
2022-11-21 17:26:55 +0100

    First scene js + styling

--- index.js
+++ index.js
@@ -11,35 +11,27 @@ function playAudio() {
     audio.play();
 }
 */
-const scene = document.getElementById('container');
 function startGame() {
+    const sceneDiv = document.getElementById('container');
     const startButton = document.getElementById('startButton');
+    const sceneImage = document.getElementById('sceneImage');
     startButton.textContent = "Starta spel";
-    startButton.addEventListener('click', loadInputDiv);
+    startButton.addEventListener('click', loadFirstScene);
 }
 
-function loadInputDiv() {
-    document.body.innerHTML = "";
-    const div = document.createElement('div');
-    div.id = "containerNew"
-    document.body.appendChild(div);
+function loadFirstScene() {
+    document.getElementById('startButton').remove();
+    document.getElementById('h1').remove();
+    document.getElementById("sceneImage").src = "./images/driving.jpg";
 
-    const inputLabel = document.createElement('h2');
-    inputLabel.textContent = "Detta spelet är läskigt och har en åldergräns!";
-    inputLabel.id = "inputLabel"
-    containerNew.appendChild(inputLabel);
+    const textBox = document.createElement('div');
+    textBox.innerHTML = "Du är på väg hem efter en lång arbetsdag. Plötsligt tänds en varningslampa på displayen och bilen blir allt svårare att köra. Du bestämmer dig för att stanna bilen åt sidan för att se vad felet är.";
+    textBox.id = "sceneText"
+    document.getElementById('container').append(textBox);
 
-    const inputP = document.createElement('p');
-    inputP.textContent = "Du måste därför ange din ålder";
-    inputP.id = "inputP"
-    containerNew.appendChild(inputP);
+    const stopCar = document.createElement('button');
+    stopCar.innerHTML = "Stanna bilen";
+    stopCar.id = "stopCar"
+    document.getElementById('container').append(stopCar);
 
-    const userAge = document.createElement('input');
-    userAge.id = "userAge"
-    containerNew.appendChild(userAge);
-
-    const submit = document.createElement('button');
-    submit.textContent = "Jag är över 18 år!";
-    submit.id = "submit"
-    containerNew.appendChild(submit);
 }
